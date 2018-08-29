d25276dba6cb98691866376045ed9225417fcb93
M. Pennella <michael.pennella@gmail.com>
2018-08-29 04:02:49 -0700

    Added basic code to create character cards

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,35 @@
 var game = {
 
+    // Data for each of the usable characters in the game, including name, picture, and combat values
+    characters: [
+        {
+            id: 'vader',
+            name: 'Darth Vader',
+            img: 'Darth_Vader.jpg',
+            atkPower: 20,
+            counterPower: 20,
+            hp: 100
+        }
+    ],
+
     // Code executed at launch - sets up initial game state
     startGame() {
         // Create characters from data and display choices in #characterSelect zone
 
-        // TESTING CODE
-        var newChar = $("<img src='assets/images/Darth_Vader.jpg' width='300px'>");
+        for (let i=0; i<this.characters.length; ++i) {
+            this.createCharDiv(this.characters[i]);
+        }
+
+    },
+
+    createCharDiv(charObj) {
+        var newChar = $("<div>");
+        newChar.append("<div>"+charObj.name+"</div>");
+        newChar.append("<img src='assets/images/"+charObj.img+"' width='300px'>");
         $("#characterSelect").append(newChar);
-        $(newDiv).on("click", function() {
+        $(newChar).on("click", function() {
             console.log("CLICKED");
         });
-
     }
 }
 
